19a3ee9ab95638fc416f45fe395792f2baf4b300
NijatS <nijatjs@code.edu.az>
2023-10-09 17:27:11 +0400

    start:githubFinder

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/main.scss";
+
+const root = document.querySelector("#root");
+
+ReactDOM.render(<h1>Hello React</h1>, root);
